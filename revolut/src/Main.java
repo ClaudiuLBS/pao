034c0a08dbf0b34bc4052498d77aa78d652759c3
Claudiu <clau_123@yahoo.com>
2023-04-06 16:14:10 +0300

    delete comments from main

--- revolut/src/Main.java
+++ revolut/src/Main.java
@@ -1,18 +1,8 @@
 import java.io.IOException;
 
 public class Main {
-
-    public static Vault c = new Vault(125d, 4d);
-
     public static MainMenu menu = MainMenu.getInstance();
-
-
     public static void main(String[] args) throws IOException{
-//        System.out.println("Working...");
-//        System.out.println(loan);
-//        System.out.println(c);
-//        System.out.println(acc);
         menu.Menu();
-
     }
 }
